81959c17c4edc2760afbc6b50bd43073c9c07739
Your Name <you@example.com>
2022-03-15 02:25:09 +0530

    mint cards section

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -6,26 +6,26 @@ const Mission = () => {
     <>
       {/* <!--================================ OUR MISSION SECTION  ================================ --> */}
       <section>
-        <div class="container py-5">
-          <div class="row d-md-flex justify-content-between align-items-center my-xxl-3">
-            <div class="col-10 col-sm-8 col-lg-5 col-xxl-4 mx-auto mx-lg-0">
-              <div class="ms-lg-4 mx-xxl-0">
-                <img src={MissionImg} class="w-100" alt="mission-img" />
+        <div className="container py-5">
+          <div className="row d-md-flex justify-content-between align-items-center my-xxl-3">
+            <div className="col-10 col-sm-8 col-lg-5 col-xxl-4 mx-auto mx-lg-0">
+              <div className="ms-lg-4 mx-xxl-0">
+                <img src={MissionImg} className="w-100" alt="mission-img" />
               </div>
             </div>
-            <div class="col-12 col-lg-6 col-xxl-5 mt-5 mt-lg-0 mb-5 mb-lg-0">
-              <div class="ps-xxl-4 text-center text-lg-start text-white">
-                <p class="para-heading mb-0">
-                  Our <span class="text-brown">Mission</span>
+            <div className="col-12 col-lg-6 col-xxl-5 mt-5 mt-lg-0 mb-5 mb-lg-0">
+              <div className="ps-xxl-4 text-center text-lg-start text-white">
+                <p className="para-heading mb-0">
+                  Our <span className="text-brown">Mission</span>
                 </p>
-                <p class="par mt-3 pt-sm-1 mb-4 mb-sm-3 mb-sm-5 pb-lg-0">
+                <p className="par mt-3 pt-sm-1 mb-4 mb-sm-3 mb-sm-5 pb-lg-0">
                   We understand that one of the biggest concerns for early-stage
                   investors is that it can quickly become stressful,
                   overwhelming, and especially extremely time consuming. It
                   isn’t easy to balance a full time job, family, and an active
                   investment portfolio, especially when just starting out.
                 </p>
-                <button class="brown-btn d-block mx-auto mx-lg-0">
+                <button className="brown-btn d-block mx-auto mx-lg-0">
                   Read More
                 </button>
               </div>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,12 +3,14 @@ import Hero from "../components/Hero";
 import Faq from "../components/Faq";
 import BackToTop from "../components/common/BackToTop";
 import Mission from "../components/Mission";
+import MintCards from "../components/MintCards";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Mission />
+      <MintCards />
       <Faq />
       <BackToTop />
     </>
