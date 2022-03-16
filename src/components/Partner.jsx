7b803de4b54f606c8a7745011bd080e9a357bead
Your Name <you@example.com>
2022-03-16 15:19:40 +0530

    partner-section

--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-// import playbit from "../assets/images/playbit.png";
-// import partnersecond from "../assets/images/partnersecond.png";
-// import trading from "../assets/images/trading.png";
 import playbit from "../assets/png/playbit-img.png";
 import partnersecond from "../assets/png/partner-24-img.png";
 import trading from "../assets/png/Trading-hive-img.png";
@@ -13,18 +10,22 @@ const Partner = () => {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <h2 className="text-center font-4xl fw-600 text-white">
+            <h2 className="text-center font-4xl fw-600 text-white py-4 py-sm-5 py-md-0">
               Part<span className="text-brown ">ners</span>
             </h2>
           </div>
         </div>
       </div>
       <section className="partner py-5">
-        <div className="container py-5">
+        <div className="container py-md-5">
           <div className="d-flex justify-content-center align-items-center partner-content">
             <div>
               <div className="py-2 playbit text-center">
-                <img className="w-100 px-4 py-2" src={playbit} alt="playbit" />
+                <img
+                  className="w-100 px-2 px-lg-4 py-2"
+                  src={playbit}
+                  alt="playbit"
+                />
               </div>
               <h6 className="font-md text-center mt-4 mb-0 fw-600">Playbit</h6>
             </div>
@@ -32,7 +33,7 @@ const Partner = () => {
             <div>
               <div className="py-2 capital text-center">
                 <img
-                  className="w-100 px-4 py-2"
+                  className="w-100 px-2 px-lg-4 py-2"
                   src={partnersecond}
                   alt="partnersecond"
                 />
@@ -45,7 +46,7 @@ const Partner = () => {
             <div>
               <div className="py-2 capital text-center">
                 <img
-                  className="w-100 px-4 py-2"
+                  className="w-100 px-2 px-lg-4 py-2"
                   src={trading}
                   alt="partnersecond"
                 />
@@ -58,7 +59,7 @@ const Partner = () => {
             <div>
               <div className="py-2 capital text-center">
                 <img
-                  className="w-100 px-4 py-2"
+                  className="w-100 px-2 px-lg-4 py-2"
                   src={focusgroup}
                   alt="partnersecond"
                 />
